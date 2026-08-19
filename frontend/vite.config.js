9fdee8236ce0dde6e70f885b95fd1f4aa5491a90
chutima290947 <chutima.co@mail.wu.ac.th>
2026-08-19 09:46:58 +0700

    fix: remove GitHib Pages base path for Render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  // สำคัญสำหรับ GitHub Pages
-  base: '/intranet/',
+  base: '/',
 
   server: {
     proxy: {
